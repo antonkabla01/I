a05e071d18ac65fc3335137c7e0fc0b81b6687eb
antonkabla01 <anton.kabla01@gmail.com>
2025-06-20 19:33:09 +0700

    first commit

---
+++ api/facebook.js
@@ -0,0 +1,54 @@
+const facebook = require('./go/facebook');
+
+module.exports = async (req, res) => {
+    const { url } = req.query;
+    if (!url) {
+        return res.end(
+            JSON.stringify({
+                status: "error",
+                message: "upss!! ada hal yang tak terduga"
+            }, null, 2)
+        );
+    }
+
+    try {
+        const extractors = [
+            ...Object.values(facebook)
+        ].filter(e => typeof e === 'function');
+
+        const extractor = extractors.find(IEClass => {
+            const instance = new IEClass();
+            return instance.VALID_URL?.test(url);
+        });
+
+        if (!extractor) {
+            return res.end(
+                JSON.stringify({
+                    status: "error",
+                    message: 'No extractor found for URL: ', url
+                }, null, 2)
+            );
+        }
+
+        const instance = new extractor();
+        const result = await instance.extract(url);
+
+        return res.end(
+            JSON.stringify(
+                {
+                    status: "success",
+                    body: result
+                }, null, 2
+            )
+        );
+    } catch (e) {
+        return res.end(
+            JSON.stringify(
+                {
+                    status: "error",
+                    message: e.message ?? e
+                }, null, 2
+            )
+        );
+    }
+};
