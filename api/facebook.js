b774e10d14613d8c8a906a4770ebaa52ce8f8dae
antonkabla01 <anton.kabla01@gmail.com>
2025-06-29 13:03:37 +0700

    Update facebook.js

--- api/facebook.js
+++ api/facebook.js
@@ -1,52 +1,86 @@
 import * as facebook from './go/facebook.js';
+import { URLSearchParams } from 'url';
 
-export default async (req, res) => {
-    const { url } = req.query;
-    if (!url) {
-        return res.end(
+const getRawBody = req =>
+   new Promise((resolve, reject) => {
+      let data = '';
+      req
+         .on('data', chunk => (data += chunk))
+         .on('end', () => resolve(data))
+         .on('error', reject);
+   });
+
+export default async function handler(req, res) {
+   try {
+      let url = '';
+
+      if (req.method === 'GET') {
+         url = req.query?.url;
+      }
+      else if (req.method === 'POST') {
+         if (req.body && typeof req.body === 'object') {
+            url = req.body.url;
+         }
+         else {
+            const raw = await getRawBody(req);
+            url = new URLSearchParams(raw).get('url');
+         }
+      }
+      else {
+         return res.end(
             JSON.stringify(
-                {
-                    code: 400,
-                    msg: "Try Again"
-                }
+               {
+                  code: 405,
+                  msg: 'Method Not Allowed'
+               }, null, 2
             )
         );
-    }
-    try {
-        const x = [...Object.values(facebook)].filter(e => typeof e === 'function');
-        const extractor = x.find(IEClass => {
-            const instance = new IEClass();
-            return instance.VALID_URL?.test(url);
-        });
-        if (!extractor) {
-            return res.end(
-                JSON.stringify(
-                    {
-                        code: 400,
-                        msg: 'No extractor found for URL: ', url
-                    }
-                )
-            );
-        }
-        const instance = new extractor();
-        const result = await instance.extract(url);
-        return res.end(
+      }
+
+      if (!url) {
+         return res.end(
             JSON.stringify(
-                {
-                    status: "success",
-                    github: "github.com/AntonThomzz",
-                    body: result
-                }
+               {
+                  code: 400,
+                  msg: 'Parameter "url" found'
+               }, null, 2
             )
-        );
-    } catch (e) {
-        return res.end(
+         );
+      }
+
+      const extractorClasses = Object.values(facebook).filter(fn => typeof fn === 'function');
+      const Extractor = extractorClasses.find(IE => new IE().VALID_URL?.test(url));
+
+      if (!Extractor) {
+         res.end(
             JSON.stringify(
-                {
-                    CODE: 404,
-                    msg: e.message ?? e
-                }
+               {
+                  code: 400,
+                  msg: `No extractor found for URL: ${url}`
+               }, null, 2
             )
-        );
-    }
-};
+         );
+      }
+
+      const result = await new Extractor().extract(url);
+      return res.end(
+         JSON.stringify(
+            {
+               status : 'success',
+               github : 'github.com/AntonThomzz',
+               body : result
+            }
+         )
+      );
+   }
+   catch (e) {
+      res.end(
+         JSON.stringify(
+            {
+               code: 500,
+               msg: e?.message || e
+            }
+         )
+      );
+   }
+}
